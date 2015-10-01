1b5df6b48a1e3cd4878214687e0cb9087eed6751
Liu Tzu Yuan <liutzuyuan@LiudeMacBook-Air.local>
2015-10-02 01:54:42 +0800

    add hw1-1 file

---
+++ hw1/hw1_1/js/main.js
@@ -0,0 +1,95 @@
+var id = 1; // unique id for list items
+
+$(document).ready(function(e) {
+	
+
+	$("tbody").on("click", ".cross", function() {
+		$(this).closest("tr").remove();
+	});
+
+	$("tbody").on("click", ".content", function() {
+		var tdObj = $(this); 
+	 	var text = tdObj.text();
+	 	var inputObj = $("<input type='text' />"); 
+	 	inputObj.val(tdObj.text());  
+	    tdObj.html(""); 
+
+	    inputObj.appendTo(tdObj);  
+	    inputObj.trigger("focus").trigger("select");
+	    inputObj.click(function() { 
+			return false; 
+		});
+		inputObj.keyup(function(event) { 
+			var keycode = event.which; 
+			if (keycode == 13) { 
+				var inputtext = $(this).val();
+				tdObj.text(inputtext); 
+			}
+			if (keycode == 27) { 
+				tdObj.html(text); 
+			}
+		}); 
+ 
+	});
+
+	$("#add").on("click", getInput);
+
+	$("#deleteall").on("click", deleteall);
+
+	$("tbody").on("click", ".box", function() {
+		$(this).closest("tr").find("span").toggleClass("checked");
+	});
+
+	$()
+
+});
+
+// triggered on Enter
+$(document).on("keydown", function(e) {
+	if(e.keyCode === 13) {
+		getInput();
+	}
+});
+
+
+
+
+// Toggle delete icon when edit button is clicked
+
+function deleteall() {
+	id = 0;
+	$("tbody").empty();
+
+};
+
+// Obtaining customer input and then calling addItem() with the input
+function getInput() {
+	var custInput = $(".custinput");
+	var input = custInput.val();
+
+	if ((input !== "") && ($.trim(input) !== "")) {
+		addItem(input);
+		custInput.val("");
+	}
+}
+
+
+/******************************************************************************
+	adding item to the list
+	increment id counter for unique id
+*******************************************************************************/
+function addItem(message) {
+
+	var d = new Date();
+	var d_string =  d.getFullYear() + "/" + (d.getMonth()+1) + "/" + d.getDate();
+	var checkbox = "<td class=\"check\">" + "<input type=\"checkbox\" id=\"item" + id + "\" class=\"box\">" + "<label for=\"item" + id + "\" class=\"check-label\"></label></td>";
+
+	var content = "<td class=\"content\"><span>" + message + "</span></td>";
+
+	var delIcon = "<td><img src=\"img/cross.png\" alt=\"cross\" class=\"cross\"></td>";
+
+	var datetime = "<td class=\"date\"><span>" + d_string + "</span></td>";
+	$("tbody").append("<tr>" + checkbox + content + datetime +delIcon  + "</tr>");
+	$(".cross").show();
+	id++;
+}
