547c23fb8b74f30e2d2a2a74e5488de72d9b4bf4
Liu Tzu Yuan <liutzuyuan@LiudeMacBook-Air.local>
2015-10-01 15:04:52 +0800

    finish hw1-3

---
+++ hw1/hw1_3.py
@@ -0,0 +1,22 @@
+#-*- coding: utf-8 -*-
+from bs4 import BeautifulSoup
+import urllib.request
+
+
+good_num = 0
+no_num = 0
+bad_num = 0
+
+htmlline=urllib.request.urlopen('https://www.ptt.cc/bbs/StupidClown/M.1443521969.A.C78.html').read() 
+soup=BeautifulSoup(htmlline,'html.parser')
+for good in soup.findAll("span", {'class' : 'hl push-tag'}):
+	good_num += 1
+for other in soup.findAll("span", {'class':'f1 hl push-tag'}):
+		
+	if other.string.strip() == '噓':
+		bad_num += 1
+	else:
+		no_num += 1
+print("推",good_num)
+print("噓",bad_num)
+print("→",no_num)
